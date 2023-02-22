19d926409409cfd7582dfd7c74aac27aff14d833
Giada Dall'Ara <giada.25700@gmail.com>
2023-02-22 18:17:53 +0100

    Esercizio 1 sulle date

--- Esercizio/src/Start.java
+++ Esercizio/src/Start.java
@@ -3,10 +3,11 @@ import java.time.Year;
 
 public class Start {
     public static void main(String[] args) {
+
         LocalDate date = LocalDate.now();
-        System.out.println("Today is " + date.getDayOfMonth() + " of the month: " + date.getMonth());
-        System.out.println((date.lengthOfMonth() - date.getDayOfMonth()) + " days left until the end of the month");
-        System.out.println((date.lengthOfYear() - date.getDayOfYear()) + " days left until the end of the year");
+        Year thisYear = Year.of(2023);
         System.out.println("We are in the year " + Year.now());
+        System.out.println((date.lengthOfMonth() - date.getDayOfMonth()) + " days left until the end of the month");
+        System.out.println((thisYear.length()-date.getDayOfYear()) + " days left until the end of the year");
     }
 }
